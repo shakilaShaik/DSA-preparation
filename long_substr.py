0ef110fd05060a7873b319692ea99b6fa560a08b
shakila shaik <145772076+shakilaShaik@users.noreply.github.com>
2025-07-28 15:50:49 +0530

    Update long_substr.py

--- long_substr.py
+++ long_substr.py
@@ -10,3 +10,30 @@ class Solution:
            long_str.add(s[right])
            max_len=max(max_len, right-left+1)
        return max_len  
+
+
+
+# Bruteforce approach
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        max_len=0
+        for i in range(len(s)):
+            new_str=''
+            for j in range(i,len(s)):
+                if s[j] in new_str:
+                    break
+                
+                else:
+                    new_str=new_str+s[j]
+                    print(new_str,"new str is")
+                
+                    new_len =len(new_str)
+                    max_len=max(max_len,new_len)
+                
+        return max_len      
+
+                
+
+
+        
