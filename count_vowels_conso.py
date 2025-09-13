95c88194614cb43d889780f5cb0b3dc3153e27c6
shakila shaik <145772076+shakilaShaik@users.noreply.github.com>
2025-09-13 11:39:02 +0530

    Update count_vowels_conso.py

--- count_vowels_conso.py
+++ count_vowels_conso.py
@@ -23,3 +23,19 @@ class Solution:
             max_count_c=max(count_c,max_count_c)
            
         return max_count + max_count_c                    
+
+
+
+
+
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        con = 0
+        vow = 0
+        d_set = set(s)
+        for i in d_set:
+            if i in "aeiou":
+                vow = max(vow, s.count(i))
+            else:
+                con = max(con, s.count(i))
+        return con+vow 
